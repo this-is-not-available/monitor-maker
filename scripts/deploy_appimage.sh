888138acd05cdc96100c183ba539dd76d32c770b
this-is-not-available <121245258+this-is-not-available@users.noreply.github.com>
2026-08-15 12:17:45 +0300

    Add AppStream metainfo

--- scripts/deploy_appimage.sh
+++ scripts/deploy_appimage.sh
@@ -9,14 +9,20 @@ BUILD_DIR="build/linux-release"
 
 rm -rf "$APPDIR"
 mkdir -p "$APPDIR"
+mkdir -p "$APPDIR/usr/share/metainfo"
 
 cmake --preset linux-release -DCMAKE_INSTALL_PREFIX=/usr/local -DMZ_BZIP2=OFF
 cmake --build "$BUILD_DIR" --parallel $(nproc)
 strip "$BUILD_DIR/monitor-maker"
 
 DESTDIR="$APPDIR" cmake --install "$BUILD_DIR"
+rDNS="io.github.this_is_not_available.portal_2_monitor_maker"
+METAINFO="$APPDIR/usr/share/metainfo/$rDNS.metainfo.xml"
+APPDATA="$APPDIR/usr/share/metainfo/$rDNS.appdata.xml"
 
-cp assets/monitor-maker.desktop "$APPDIR/monitor-maker.desktop"
+cp assets/monitor-maker.desktop "$APPDIR/$rDNS.desktop"
+cp assets/appstream.metainfo.xml "$METAINFO"
+cp assets/appstream.metainfo.xml "$APPDATA"
 cp assets/MonMakeLogo_512.png "$APPDIR/MonMakeLogo_512.png"
 
 if [ ! -f linuxdeploy.AppImage ]; then
@@ -29,7 +35,7 @@ if [ -n "${DEPLOY_APPIMAGE_FILENAME:-}" ]; then
 fi
 ./linuxdeploy.AppImage --appimage-extract
 ./squashfs-root/AppRun --appdir "$APPDIR" \
-  --desktop-file="$APPDIR/monitor-maker.desktop" \
+  --desktop-file="$APPDIR/$rDNS.desktop" \
   --icon-file="$APPDIR/MonMakeLogo_512.png" \
   --executable "$APPDIR/usr/local/bin/monitor-maker" \
   --output appimage
